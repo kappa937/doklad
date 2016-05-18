dca0e0787fe6565dfe43197f6fbd28d5694424f3
kappa937 <kappa937@gmail.com>
2016-05-18 13:39:42 +0300

    test5

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 	include_once "comma.php";
 	$result = Comma::update('Hello, world, world, Hello, world, Hello', '');
 	
-	if ($result == 'Hello world world Hello world Hello') {
+	if ($result != 'Hello world world Hello world Hello') {
 		echo 'Check OK.';
 	} else {
 		throw new Exception('Check error.');
