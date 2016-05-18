e0db501ef2fce930f9fbe2a4d84bb2b87aa27454
kappa937 <kappa937@gmail.com>
2016-05-18 13:33:08 +0300

    test3

--- test.php
+++ test.php
@@ -1,9 +1,7 @@
-<?php
-	include_once "comma.php";
-	
-	if (Comma::update('Hello, world, world, Hello, world, Hello')) {
-		echo 'Check OK.';
-	} else {
-		echo 'Check ERROR.';
-	}
+<?php<br>$fixture = array();<br>
+assertTrue(count($fixture) == 0);<br> <br>$fixture[] = 'element';<br>
+assertTrue(count($fixture) == 1);<br> <br>function assertTrue($condition)<br>
+{<br>  if (!$condition) {<br>    
+throw new Exception('Assertion failed.');<br>
+  }<br>}<br>?><br><br>
 ?>
